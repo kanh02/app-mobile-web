8741b02cb53371f0540eb06a4b58fcfceabb8bb5
oscar <kajiryohji02@gmail.com>
2018-09-30 19:07:05 +0200

    refresh

--- public/js/powerful.js
+++ public/js/powerful.js
@@ -1,6 +1,11 @@
+document.getElementById("login").addEventListener("click", login);
+document.getElementById("logout").addEventListener("click", logout);
+document.getElementById("submitmsg").addEventListener("click", writeNewPost);
+
+var user = firebase.auth().currentUser;
 
 
-fetch("https://api.jsonbin.io/b/5b977d6fd6fe677c48d896d6", {
+fetch("https://api.myjson.com/bins/bv1v8", {
 	method: "GET",
 
 }).then(function (response) {
@@ -22,7 +27,6 @@ fetch("https://api.jsonbin.io/b/5b977d6fd6fe677c48d896d6", {
 	f4();
 	f5();
 	f6();
-//	button();
 
 }).then(function () {
 
@@ -30,9 +34,18 @@ fetch("https://api.jsonbin.io/b/5b977d6fd6fe677c48d896d6", {
 	console.log("Request failed: " + error.message);
 })
 
+var btn = document.getElementById("chatbtn");
+btn.addEventListener("click", showChat);
 
-///////////////////////////////////////////////funciones/////////////////////////////////////////////////////////
+function showChat() {
+	var menu = document.getElementById("botmenu");
+	var chat = document.getElementById("chatsection");
+	menu.style.display = "none";
+	chat.style.display = "block";
 
+}
+
+//////////////////////////////////////////////////funciones///////////////////////////////////////////////////////////
 
 function lista() {
 	var list = document.getElementById("myUL");
@@ -44,6 +57,7 @@ function lista() {
 		complete.setAttribute("data-location", data[a].direccion)
 		complete.setAttribute("data-name", data[a].equipo1.nombre)
 		complete.setAttribute("data-name2", data[a].equipo2.nombre)
+		complete.setAttribute("data-url", data[a].url)
 
 		complete.textContent = data[a].equipo1.nombre + " VS " + data[a].equipo2.nombre;
 
@@ -51,21 +65,17 @@ function lista() {
 	}
 }
 
-
 function table1() {
 
 	var click = document.getElementById("myUL");
-
 	click.onclick = function () {
-		
-//falta el onclick que borre la tabla de arriba//
+
 		var list = document.getElementById("list");
-		list.innerHTML= "";
-
+		list.innerHTML = "";
 		var z = event.target.getAttribute("data-date");
-
 		console.log(z)
-		
+
+		var map = document.getElementById("mapa");
 		var equip = document.createElement("li");
 		var equip2 = document.createElement("li");
 		var date = document.createElement("li");
@@ -74,23 +84,25 @@ function table1() {
 
 
 		equip.textContent = event.target.getAttribute("data-name");
-		list.appendChild(equip);
-
 		equip2.textContent = event.target.getAttribute("data-name2");
-		list.appendChild(equip2);
-
 		date.textContent = event.target.getAttribute("data-date");
-		list.appendChild(date);
-
-		site.textContent = event.target.getAttribute("data-location");
-		list.appendChild(site)
-
 		time.textContent = event.target.getAttribute("data-time");
+		site.textContent = event.target.getAttribute("data-location");
+		map.src = event.target.getAttribute("data-url");
+		
+		
+		document.getElementById("primer").style.display = "none";
+		
+		
+		list.appendChild(equip);
+		list.appendChild(equip2);
+		list.appendChild(date);
 		list.appendChild(time);
+		list.appendChild(site);
+		list.style.display = "block";
+		document.getElementById("dinamical").style.display="block";
 
 	}
-	/////////pendiente el remove///////////////
-
 }
 
 function lista2() {
@@ -126,16 +138,12 @@ function lista2() {
 			listaul2.appendChild(jugadores2);
 		}
 		lista2.appendChild(listaul2);
-
-
-
-
 		list.appendChild(lista);
 		list2.appendChild(lista2);
 	}
 }
 
-////////////////////////////filtro//////////////////////////////////////////////
+///////////////////////////////////////////////////////filtro//////////////////////////////////////////////////////////
 
 function filtro() {
 
@@ -154,7 +162,7 @@ function filtro() {
 	}
 }
 
-/////////////////////////////show and hide//////////////////////////////////////////////////
+////////////////////////////////////////////////show and hide//////////////////////////////////////////////////
 
 function f1() {
 	var x = document.getElementById("primer");
@@ -236,11 +244,178 @@ function f6() {
 		z.style.display = "block";
 	}
 }
-function button(){
-	var z = document.getElementById("botmenu");
-	if(z.style.display === "none")
-		z.style.display = "block";
-	else{
-		z.style.display = "none";
+
+function showlist(){
+ var lis =document.getElementById("dinamical");
+ var lisd =document.getElementById("myUL");
+	if(lis.style.dislay = "block"){
+	   lisd.style.dislay = "none";
 	}
 }
+
+function button() {
+	var z = document.getElementById("botmenu");
+
+	var a = document.getElementById("primer");
+	var b = document.getElementById("segundo");
+	var c = document.getElementById("tercero");
+	var d = document.getElementById("cuarto");
+	var e = document.getElementById("quinto");
+	var g = document.getElementById("info");
+	var h = document.getElementById("list");
+	var i = document.getElementById("chatsection");
+	var j = document.getElementById("dinamical");
+
+	z.style.display = "block";
+	a.style.display = "none";
+	b.style.display = "none";
+	c.style.display = "none";
+	d.style.display = "none";
+	e.style.display = "none";
+	g.style.display = "none";
+	h.style.display = "none";
+	i.style.display = "none";
+	j.style.display = "none";
+}
+
+var btn = document.getElementById("chatbtn");
+btn.addEventListener("click", showChat);
+
+function showChat() {
+	var menu = document.getElementById("botmenu");
+	var chat = document.getElementById("chatsection");
+	var a = document.getElementById("primer");
+	var b = document.getElementById("segundo");
+	var c = document.getElementById("tercero");
+	var d = document.getElementById("cuarto");
+	var e = document.getElementById("quinto");
+	var g = document.getElementById("info");
+	var h = document.getElementById("list");
+	var i = document.getElementById("dawn");
+	
+	menu.style.display = "none";
+	chat.style.display = "block";
+	a.style.display = "none";
+	b.style.display = "none";
+	c.style.display = "none";
+	d.style.display = "none";
+	e.style.display = "none";
+	g.style.display = "none";
+	h.style.display = "none";
+	i.style.display = "none";
+
+}
+
+///////////////////////////////////////////////////////chat////////////////////////////////////////////////////
+
+firebase.auth().onAuthStateChanged(function (user) {
+	if (user) {
+		getPosts();
+	} else {
+		var posts = document.getElementById("posts");
+		posts.innerHTML = "";
+
+	}
+});
+
+function login() {
+	var provider = new firebase.auth.GoogleAuthProvider();
+	firebase.auth().signInWithPopup(provider)
+		.then(function () {
+			console.log(firebase.auth());
+		})
+		.catch(function () {
+			alert("Something went wrong");
+		});
+}
+
+function logout() {
+
+	firebase.auth().signOut()
+		.then(function () {})
+		.catch(function (error) {});
+}
+
+function writeNewPost() {
+
+	var text = document.getElementById("textInput").value;
+
+	var date = new Date();
+	var date1 = date.getHours();
+	var date2 = date.getMinutes();
+	if (date2 < 10) {
+		date2 = "0" + date2;
+	}
+	var p = ":";
+	var fecha = date1 + p + date2;
+
+
+	if (firebase.auth().currentUser == null) {
+		alert(" little grasshopper login ^_^");
+	} else {
+
+		var userName = firebase.auth().currentUser.displayName;
+		var post = {
+			name: userName,
+			body: text,
+			date: fecha
+		};
+
+		var newPostKey = firebase.database().ref().child('general').push().key;
+
+		var updates = {};
+		updates[newPostKey] = post;
+
+		document.getElementById("textInput").value = "";
+		return firebase.database().ref('general').update(updates);
+	}
+}
+
+function getPosts() {
+
+	if (firebase.auth().currentUser == null) {
+		alert("you must be logued");
+	} else {
+
+		firebase.database().ref('general').on('value', function (data) {
+			var posts = document.getElementById("posts");
+			posts.innerHTML = "";
+
+			var messages = data.val();
+
+			for (var key in messages) {
+
+				var text = document.createElement("div");
+				var name = document.createElement("h4");
+				var date = document.createElement("h4");
+
+				var element = messages[key];
+
+
+				if (firebase.auth().currentUser.displayName == element.name) {
+					text.setAttribute("class", "local");
+
+				} else if (firebase.auth().currentUser.displayName != element.name) {
+					text.setAttribute("class", "guest");
+				}
+
+				date.append(element.date);
+				name.append(element.name);
+
+				text.append(name);
+				text.append(element.body);
+				text.append(date);
+				posts.append(text);
+
+
+			}
+		})
+	}
+
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//function updateScroll() {
+//       var element = document.getElementById("post");
+//       var elementHeight = element.scrollHeight;
+//       element.scrollTop = elementHeight
+//}
